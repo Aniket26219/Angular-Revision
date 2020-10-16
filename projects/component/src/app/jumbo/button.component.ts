a949d29b8d40fbd28a385b92b495d8ab094c3221
ANIKET <aniket98shahane@gmail.com>
2020-10-16 12:26:56 +0530

    View Encapsulation preparation

--- projects/component/src/app/jumbo/button.component.ts
+++ projects/component/src/app/jumbo/button.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   template: `
     <div class="btn btn-outline-primary">Sing Up</div>
     <div class="btn btn-outline-info">Sing In</div>
+    <p>This card is only for example purpose only. --> In Button</p>
     <!-- This component is created by using the command ng g c jumbo/button -it -is --flat --project component -->
   `,
   styles: [
