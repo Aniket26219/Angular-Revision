2e0ac8020db4129dc6c3eef444c5bb3cf8abf435
ANIKET <aniket98shahane@gmail.com>
2020-10-21 13:22:41 +0530

    some improvements

--- projects/cust-comp/src/app/custom-directive/custom-directive.component.ts
+++ projects/cust-comp/src/app/custom-directive/custom-directive.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomDirectiveComponent implements OnInit {
 
+  mobiles = ['Redmi', 'Samsung', 'Realme', 'OnePlus', 'Iphone']
+
   constructor() { }
 
   ngOnInit(): void {
