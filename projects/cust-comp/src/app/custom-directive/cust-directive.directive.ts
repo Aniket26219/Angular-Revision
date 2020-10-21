55c8c07f1888043b81de5ac7afd48dba4e8f1615
ANIKET <aniket98shahane@gmail.com>
2020-10-21 12:55:53 +0530

    custom directive custom inputs

--- projects/cust-comp/src/app/custom-directive/cust-directive.directive.ts
+++ projects/cust-comp/src/app/custom-directive/cust-directive.directive.ts
@@ -1,10 +1,36 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Renderer2, OnInit, Output, Input } from '@angular/core';
 
 @Directive({
   selector: '[appCustDirective]'
 })
 export class CustDirectiveDirective {
 
-  constructor() { }
+  @Input('appCustDirective')
+  @HostBinding('class')
+  cl : string
+
+  @HostBinding('style.font-size.px')
+  font : number
+
+  constructor(
+    private elemtref : ElementRef,
+    private render : Renderer2
+  ) { 
+    // elemtref.nativeElement.style.border = '1px solid red' //not recommended
+    // render.setStyle(elemtref.nativeElement, 'border', '1px solid red')
+    this.cl = `alert alert-warning`
+  }
+
+  @HostListener('mouseenter')
+  OnInit(){
+    this.cl = `alert alert-success`
+    this.font = 25
+  }
+
+  @HostListener('mouseleave')
+  Output(){
+    this.cl = `alert alert-warning`
+    this.font = 16
+  }
 
 }
